c60a3f25683419854614a9dfbdd2210d5dd5903f
Ercilla Verzosa, Wilfredo <wilfredo.ercilla@ciberexperis.es>
2018-01-29 12:11:11 +0100

    Se corrige alerta sonar en clase Ticket, ya que según alerta hay que declararla como interface o bien debe tener contenido

--- src/main/java/com/mycorp/support/Ticket.java
+++ src/main/java/com/mycorp/support/Ticket.java
@@ -1,7 +1,14 @@
 package com.mycorp.support;
 
-
-
 public class Ticket {
+    String ticket;
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
 
 }
